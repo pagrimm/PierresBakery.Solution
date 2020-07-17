52584638b16c043ba6bd865c46ae71084df6cbbb
Peter Grimm <pagrimm@gmail.com>
2020-07-17 09:51:09 -0700

    add passing tests for getting price of bread and pastry objects

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -6,9 +6,12 @@ namespace PierresBakery.Models
   public class Bread
   {
     public string Type { get; }
+    public int Price { get; }
+    private Dictionary<string, int> _priceList = new Dictionary<string, int>() { {"Default", 1}, {"French", 2} };
     public Bread(string inputType)
     {
       Type = inputType;
+      Price = _priceList[Type];
     }
   }
 }
--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -7,9 +7,12 @@ namespace PierresBakery.Models
   public class Pastry
   {
     public string Type { get; }
+    public int Price { get; }
+    private Dictionary<string, int> _priceList = new Dictionary<string, int>() { {"Default", 1}, {"Donut", 2} };
     public Pastry(string inputType)
     {
       Type = inputType;
+      Price = _priceList[Type];
     }
   }
 }
