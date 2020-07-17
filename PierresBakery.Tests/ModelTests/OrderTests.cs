9f61ac5568f5cf6da53f8193ee68226f2e219bfd
Peter Grimm <pagrimm@gmail.com>
2020-07-17 10:14:07 -0700

    add failing tests for getting bread and pastry lists from order object

--- PierresBakery.Tests/ModelTests/OrderTests.cs
+++ PierresBakery.Tests/ModelTests/OrderTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PierresBakery.Models;
+using System.Collections.Generic;
 
 namespace PierresBakery.Tests
 {
@@ -8,11 +9,34 @@ namespace PierresBakery.Tests
   {
 
     [TestMethod]
-    public void ClassConstructor_CreatesInstanceOfClass_Class()
+    public void OrderConstructor_CreatesInstanceOfClass_Order()
     {
       Order newOrder = new Order();
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesFieldOfBreadList_List()
+    {
+      Order newOrder = new Order();
+      Assert.AreEqual(typeof(List<Bread>), newOrder.BreadList.GetType());
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesFieldOfPastryList_List()
+    {
+      Order newOrder = new Order();
+      Assert.AreEqual(typeof(List<Pastry>), newOrder.PastryList.GetType());
+    }
+
+    /* [TestMethod]
+    public void AddBread_AddsBreadObjectToOrder_Object()
+    {
+      Order newOrder = new Order();
+      Bread newBread = new Bread("French");
+      newOrder.AddBread(newBread);
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    } */
 
   }
 }
